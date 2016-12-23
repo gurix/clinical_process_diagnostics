a1c8fcb99190b8f6ce79809f3c058ced29b8f241
Markus Graf <info@markusgraf.ch>
2016-12-23 13:44:34 +0100

    - Ability of display two colors -ms-fill-upper and -ms-fill-lower for webkit and firefox for range inputs - Hide range input tooltip on IE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,36 @@
 //= require bootstrap-slider
 //= require chartist.min
 //= require_tree .
+
+// Style the track of a range input with a different lower and an upper color. Fixed missing -ms-fill-upper and -ms-fill-lower on webkit and firefox
+$(document).ready(function(){
+  var r = document.querySelectorAll('input[type=range]'), 
+    prefs = ['webkit-slider-runnable', 'moz-range'], 
+    styles = [], 
+    l = prefs.length,
+    n = r.length;
+
+  var getTrackStyleStr = function(el, j) {
+    var str = '', 
+        min = el.min || 0, 
+        perc = (el.max) ? ~~(100*(el.value - min)/(el.max - min)) : el.value, 
+        val = perc + '%',
+        id = el.id;
+    for(var i = 0; i < l; i++) {
+      str += '#' + id + '::-' + prefs[i] + '-track{ background: linear-gradient(90deg, #5cbdb1 ' + val + ', #DDD ' + val + ');} ';
+    }
+    return str;
+  };
+
+  var setDragStyleStr = function(evt) {
+    var trackStyle = getTrackStyleStr(evt.target, this + 1);  
+    styles[this].textContent = trackStyle;
+  };
+
+  for(var i = 0; i < n; i++) {
+    var s = document.createElement('style');
+    document.body.appendChild(s);
+    styles.push(s);  
+    r[i].addEventListener('input', setDragStyleStr.bind(i), false);
+  }
+});
