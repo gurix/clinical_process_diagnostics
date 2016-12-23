886066a94760dd7bbb36dff4088e128155d245a4
Markus Graf <info@markusgraf.ch>
2016-12-23 11:26:43 +0100

    Adjusted content and styling

--- app/models/survey/session_rating_scale.rb
+++ app/models/survey/session_rating_scale.rb
@@ -11,11 +11,11 @@ module Survey
     belongs_to :therapist, inverse_of: :session_rating_scale_sessions
 
     # The real survey fields
-    field :relationship, type: Integer
-    field :goals_and_topics, type: Integer
-    field :approach_or_method, type: Integer
-    field :overall, type: Integer
-    field :coping, type: Integer # Extension of 3th version from Prof. Dr. Marcel Schaer Gmelch
+    field :relationship,       type: Integer, default: 0
+    field :goals_and_topics,   type: Integer, default: 0
+    field :approach_or_method, type: Integer, default: 0
+    field :overall,            type: Integer, default: 0
+    field :coping,             type: Integer, default: 0 # Extension of 3th version from Prof. Dr. Marcel Schaer Gmelch
 
     validates :relationship,       presence: true
     validates :goals_and_topics,   presence: true
