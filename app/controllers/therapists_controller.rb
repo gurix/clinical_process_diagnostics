57af5a7a0bfd804c136cfb39d95cb8c835c3847c
Markus Graf <info@markusgraf.ch>
2016-12-28 11:29:21 +0100

    Protect index page with a password

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,4 +1,6 @@
 class TherapistsController < ApplicationController
+  http_basic_authenticate_with name: "", password: ENV['ADMIN_PASS'], only: :index
+
   def index
     @therapists = Therapist.asc(:name)
   end
