94fd5a96fb597e351b8e73122119a36ee448e91b
Markus Graf <info@markusgraf.ch>
2016-12-28 11:16:27 +0100

    Updated seeds, just for demonstration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Therapist.create(name: 'Markus Graf', email: 'markus.graf@zhaw.ch')
-Therapist.create(name: 'Dr. Paul Weston', email: 'paul.weston@intreatment.movie')
+Therapist.create(name: 'Markus Graf', email: 'markus.graf@zhaw.ch', token: 'alis')
+Therapist.create(name: 'Prof. Dr. Marcel Schär Gmelch', email: 'marcel.schaer@zhaw.ch', token: 'lsksk')
+Therapist.create(name: 'Dr. Paul Weston', email: 'paul.weston@intreatment.movie', token: 'osas')
