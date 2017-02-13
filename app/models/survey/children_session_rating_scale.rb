9ddb27c652d1859c518795a4dfd8c9c447924fd5
Markus Graf <info@markusgraf.ch>
2017-02-13 13:33:58 +0100

    Set cursor to the middle

--- app/models/survey/children_session_rating_scale.rb
+++ app/models/survey/children_session_rating_scale.rb
@@ -5,11 +5,11 @@ module Survey
     validates :version, presence: true
 
     # The real survey fields
-    field :relationship,       type: Integer, default: 0
-    field :goals_and_topics,   type: Integer, default: 0
-    field :approach_or_method, type: Integer, default: 0
-    field :overall,            type: Integer, default: 0
-    field :coping,             type: Integer, default: 0 # Extension of 3th version from Prof. Dr. Marcel Schaer Gmelch
+    field :relationship,       type: Integer, default: 50
+    field :goals_and_topics,   type: Integer, default: 50
+    field :approach_or_method, type: Integer, default: 50
+    field :overall,            type: Integer, default: 50
+    field :coping,             type: Integer, default: 50 # Extension of 3th version from Prof. Dr. Marcel Schaer Gmelch
 
     validates :relationship,       presence: true
     validates :goals_and_topics,   presence: true
